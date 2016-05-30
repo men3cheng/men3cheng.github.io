eaa13fc467d6c96a32748745ad9a5c806dfd09b8
men3cheng <men3cheng@163.com>
2016-05-31 00:05:15 +0800

    接着码。。。

--- zns/class/js/move.js
+++ zns/class/js/move.js
@@ -1,31 +1,43 @@
-function getStyle(obj,name){
+function getStyle(obj,name)
+{
 	if(obj.currentStyle){
 		return obj.currentStyle[name];
 	}else{
 		return getComputedStyle(obj,false)[name];
 	}
 }
-function startMove(obj,attr,iTarget,fnEnd){
+function startMove(obj,json,fnEnd)
+{
 	clearInterval(obj.timer);
-	obj.timer=setInterval(function(){
-		var cur=0;
-		if(attr=='opacity'){
-			cur=Math.round(parseFloat(getStyle(obj,attr))*100);					
-		}else{
-			cur=parseInt(getStyle(obj,attr));
-		}
-		var speed=(iTarget-cur)/6;
-		speed=speed>0?Math.ceil(speed):Math.floor(speed);
-		if(cur==iTarget){
-			clearInterval(obj.timer);
-			if(fnEnd)fnEnd();  //当该对象传进来的时候才去调用；
-		}else{
+	obj.timer=setInterval(function()
+	{
+		var bStop=true; //假设所有的值都已经准备完毕；
+		for(var attr in json)
+		{
+			var cur=0;
+			if(attr=='opacity')
+			{
+				cur=Math.round(parseFloat(getStyle(obj,attr))*100);					
+			}else{
+				cur=parseInt(getStyle(obj,attr));
+			}
+			var speed=(json[attr]-cur)/10;
+			speed=speed>0?Math.ceil(speed):Math.floor(speed);
+			
+			if(cur!=json[attr])
+				bStop=false;
+			
 			if(attr=='opacity'){
 				obj.style.filter='alpha(opacity:'+(cur+speed)+')';
 				obj.style.opacity=(cur+speed)/100;	
 			}else{
 				obj.style[attr]=cur+speed+'px';
-			}					
+			}
 		}
-	},30)
+		if(bStop)
+		{
+			clearInterval(obj.timer);
+			if(fnEnd)fnEnd();
+		}
+	},30);
 }
