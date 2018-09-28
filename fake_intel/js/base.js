3e0702c1c3b31afb15deb9e5ee2bab360641f3f3
men3cheng <men3cheng@163.com>
2018-09-28 09:15:21 +0800

    del fake_intel

--- fake_intel/js/base.js
+++
@@ -1,9 +0,0 @@
-/********Bootlint*******/
-(function () {
-  var s = document.createElement("script");
-  s.onload = function () {
-    bootlint.showLintReportForCurrentDocument([]);
-  };
-  s.src = "js/bootlint.min.js";
-  document.body.appendChild(s)
-})();
